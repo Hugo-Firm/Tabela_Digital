a3774d0af0ce8ad4a020905d365e9c0212d5d8e6
Hugo-Firm <hugofffm@gmai.com>
2026-08-10 21:15:07 -0300

    sou muito foda

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { CharacterCard } from './components/CharacterCard'
+import CharacterGrid from './components/CharacterCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <CharacterCard characters={{ name: "Elyndra Nightsong", race: "Elfa", characterClass: "Mago", level: 7 }} />
+      <CharacterGrid />
     </>
   )
 }
